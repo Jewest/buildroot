5bade9689bb527ac5faa99fda821c6483d25b563
Leo Soares <leojrfs@gmail.com>
2017-06-13 23:02:10 +0100

    first commit

---
+++ board/orangepi/orangepi-2g-iot/boot.cmd
@@ -0,0 +1,21 @@
+if test "${boot_device}" = "mmc"; then
+
+	setenv rootdev "/dev/mmcblk0p2"
+	setenv rootfstype "ext4"
+
+	setenv bootargs "root=${rootdev} rootwait rootfstype=${rootfstype} console=ttyS0,921600 kgdboc=ttyS0,921600 panic=10 consoleblank=0 earlyprintk"
+
+	fatload mmc 0:1 ${kernel_addr} zImage
+	fatload mmc 0:1 ${modem_addr} modem.bin
+	fatload mmc 0:1 ${factorydata_addr} factorydata.img
+
+else
+	echo "NAND boot is not implemented yet"
+fi
+
+# modem init is mandatory according to u-boot sources
+mdcom_cal_loadm ${factorydata_addr}
+mdcom_loadm ${modem_addr}
+mdcom_check 1
+
+bootz ${kernel_addr}
